315f64c4e2352d6718fe3b91dc6351f1fb39888b
tarikulislam <tarikulislam4145@gmail.com>
2025-01-15 13:36:30 +0530

    Update

---
+++ app/Auth/App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import twrnc from 'twrnc'
+import React from 'react'
+
+const App = () => {
+    return (
+        <View>
+            <Text>App</Text>
+        </View>
+    )
+}
+
+export default App
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -57,3 +57,26 @@ function RootLayoutNav() {
     </ThemeProvider>
   );
 }
+
+
+
+
+
+
+
+
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// // import App from Auth/App
+// import App from './Auth/App'
+
+// const _layout = () => {
+//   return (
+//     <View>
+
+//       <App />
+//     </View>
+//   )
+// }
+
+// export default _layout
